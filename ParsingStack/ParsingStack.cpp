42622691d3700837b49d243e3e7847cfcea5ee60
Kamil Plich <kamilplich2@gmail.com>
2018-11-10 23:19:04 +0100

    created parsing stack

---
+++ ParsingStack/ParsingStack.cpp
@@ -0,0 +1,47 @@
+﻿#include "ParsingStack.h"
+#include <string>
+
+ParsingStack::ParsingStack(std::string string)
+{
+	//deallocated in destructor
+	this->source = new std::list<char>(string.begin(), string.end());
+	this->position_counter = 0;
+}
+
+ParsingStack::~ParsingStack()
+{
+	delete source;
+}
+
+char ParsingStack::pop_one()
+{
+	char first = source->front();
+	source->pop_front();
+
+	++position_counter;
+
+	return first;
+}
+
+std::string ParsingStack::pop_until_char_found(char ending_character)
+{
+	std::string result;
+
+	char current_top = pop_one();
+
+	while(current_top != ending_character)
+	{
+		result += current_top;
+		current_top = pop_one();
+	}
+
+	return result;
+}
+
+int ParsingStack::get_position()
+{
+	return position_counter;
+}
+
+
+
---
+++ ParsingStack/ParsingStack.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <list>
+
+/**
+ * Class simplifying parsing a string and keeping track of position in the string.
+ */
+class ParsingStack
+{
+public:
+	ParsingStack(std::string string);
+	~ParsingStack();
+
+	/**
+	 * Returns a character at the top of the stack
+	 * @return a character at the top of the stack
+	 */
+	char pop_one();
+
+	/**
+	 * Returns a string based on chars popped until given character has been found.
+	 * After returning the string, the stack doesn't contain the ending character.
+	 * @param ending_character character that's supposed to terminate popping the characters
+	 * @return a string containing all the characters popped until ending character has been found
+	 * (the returned string doesn't contain the ending character)
+	 */
+	std::string pop_until_char_found(char ending_character);
+
+	/**
+	 * Returns current position in the input string.
+	 * @return current position in the input string
+	 */
+	int get_position();
+
+private:
+	/**
+	 * Source of chars.
+	 */
+	std::list<char>* source;
+
+	/**
+	 * Number indicating position in the string.
+	 */
+	int position_counter;
+};
